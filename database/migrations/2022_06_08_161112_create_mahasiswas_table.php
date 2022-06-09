579252ae26f8b441efd1fdb849a0c348feba1ce3
Andre <andrian41210.pesonaxxi@gmail.com>
2022-06-09 16:39:53 +0700

    Membuat model dan navbar

---
+++ database/migrations/2022_06_08_161112_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nrp', 50);
+            $table->string('nama', 50);
+            $table->string('email', 100);
+            $table->string('alamat', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@include('layouts.main')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Selamat Datang di Simple Siakad</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quidem.
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
